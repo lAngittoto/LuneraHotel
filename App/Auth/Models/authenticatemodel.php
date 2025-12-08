63f87cf27b4a4b832c24939d4485c9854884ff6d
Lan Gitto <git762647@gmail.com>
2025-12-08 19:29:19 +0800

    Add new project folder

--- App/Auth/Models/authenticatemodel.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getUser($pdo, $email) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
-    $stmt->execute([$email]);
-    return $stmt->fetch(PDO::FETCH_ASSOC);
-}
